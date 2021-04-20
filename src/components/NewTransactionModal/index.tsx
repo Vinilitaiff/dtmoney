337ea1ee3e87db2ebcff951cf443bae125f2132f
Vinicius <vinilitaiff@gmail.com>
2021-04-20 01:30:56 -0400

    impl do radiobox

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from 'react-modal';
-import { Container, TransactionTypeContainer } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImage from '../../assets/close.svg';
+import { useState } from 'react';
 
 interface NewTransactionModalProps{
   isOpen: boolean;
@@ -10,6 +11,8 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({isOpen,onRequestClose}:NewTransactionModalProps){
+  const [type, setType] = useState('deposit');
+
   return(
     <Modal 
       isOpen={isOpen}
@@ -28,19 +31,31 @@ export function NewTransactionModal({isOpen,onRequestClose}:NewTransactionModalP
          <h2>Cadastrar transação</h2>       
          <input type="text" placeholder="Titulo"/>
          <input type="number" placeholder="Valor"/>
+
          <TransactionTypeContainer>
-           <button 
-             type="button">
+          
+           <RadioBox 
+             type="button"             
+             onClick={()=> { setType('deposit'); }}
+             isActive={type === 'deposit'}
+             activeColor="green"
+           >
              <img src={incomeImg} alt="Entrada"/>
              <span>Entrada</span>
-           </button>
+           </RadioBox>
 
-           <button 
-             type="button">
+           <RadioBox 
+             type="button"
+             onClick={()=> { setType('withdraw'); }}
+             isActive={type === 'withdraw'}
+             activeColor="red"
+           >
              <img src={outcomeImg} alt="Saída"/>
              <span>Saída</span>
-           </button>
+           </RadioBox>
+
          </TransactionTypeContainer>
+
          <input placeholder="Categoria"/>
          <button type="submit">Cadastrar</button>
       </Container>
